5f405eabf0d9aae650b35e35993fad1f331b9bca
BanseokSuh <still3028@gmail.com>
2024-01-10 22:28:20 +0900

    [Backend] cors 이슈 해결

---
+++ src/main/java/com/banny/blog/global/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.banny.blog.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("http://localhost:3000") // localhost:3000 url만
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
