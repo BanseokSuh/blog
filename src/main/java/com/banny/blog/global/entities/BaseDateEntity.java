4d6b3a02aaf6d8cea60ef30d84d129f359942dda
BanseokSuh <still3028@gmail.com>
2024-01-12 00:50:36 +0900

    [Backend] PostSearchRequest Dto 생성, querydsl 의존성 주입

--- src/main/java/com/banny/blog/global/entities/BaseDateEntity.java
+++ src/main/java/com/banny/blog/global/entities/BaseDateEntity.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -13,9 +12,9 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Getter
-@MappedSuperclass // BaseEntity를 상속받은 엔티티들은 아래 필드들을 컬럼으로 인식
+@MappedSuperclass // BaseDateEntity를 상속받은 엔티티들은 아래 필드들을 컬럼으로 인식
 @EntityListeners(AuditingEntityListener.class) // 자동으로 값 매핑 기능 추가
-public abstract class BaseEntity {
+public abstract class BaseDateEntity {
 
     @CreatedDate
     @Column(updatable = false, nullable = false)
