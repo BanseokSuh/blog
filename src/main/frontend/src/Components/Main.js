ecba9136e083c17cd2243853a2ccbf337976808c
BanseokSuh <still3028@gmail.com>
2024-01-22 22:10:17 +0900

    [Frontend] main 컴포넌트 분리

--- src/main/frontend/src/Components/Main.js
+++ src/main/frontend/src/Components/Main.js
@@ -1,18 +1,5 @@
-import Experience from "./Experience/Experience";
-import Brief from "./Brief";
-import Introduce from "./Introduce";
-
 const Main = () => {
-  return (
-    <>
-      {/* 간략한 정보 영역 */}
-      <Brief />
-      {/* 소개 영역 */}
-      <Introduce />
-      {/* 경력사항 영역 */}
-      <Experience />
-    </>
-  );
+  return <>메인</>;
 };
 
 export default Main;
