1c2e6c8a1506c152cbd556ef67b8e78d6db0c8dd
BanseokSuh <still3028@gmail.com>
2024-02-04 13:16:01 +0900

    [Backend/refactor] BaseEntity 수정

--- src/main/java/com/banny/blog/global/entities/BaseEntity.java
+++ src/main/java/com/banny/blog/global/entities/BaseEntity.java
@@ -3,6 +3,7 @@ package com.banny.blog.global.entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Builder;
 import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
@@ -12,19 +13,27 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Getter
-@MappedSuperclass // BaseDateEntity를 상속받은 엔티티들은 아래 필드들을 컬럼으로 인식
+@MappedSuperclass // BaseEntity를 상속받은 엔티티들은 아래 필드들을 컬럼으로 인식
 @EntityListeners(AuditingEntityListener.class) // 자동으로 값 매핑 기능 추가
-public abstract class BaseDateEntity {
+public abstract class BaseEntity {
 
     @Getter
     @CreatedDate
     @Column(updatable = false, nullable = false)
     @CreationTimestamp
-    private LocalDateTime createdDate;
+    private LocalDateTime createdAt;
+
+    @Column(updatable = false, nullable = false)
+    @Builder.Default
+    private Long createdBy = 1L;
 
     @Getter
     @LastModifiedDate
     @Column(nullable = false)
     @CreationTimestamp
-    private LocalDateTime modifiedDate;
+    private LocalDateTime modifiedAt;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Long modifiedBy = 1L;
 }
