a51a2618e0d223c088f493947e4b9cdc24219641
BanseokSuh <still3028@gmail.com>
2024-02-13 20:57:06 +0900

    [Backend/refactor] User entity 생성자 추가

--- src/main/java/com/banny/blog/domain/user/domain/User.java
+++ src/main/java/com/banny/blog/domain/user/domain/User.java
@@ -2,6 +2,7 @@ package com.banny.blog.domain.user.domain;
 
 import com.banny.blog.global.entities.BaseEntity;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -38,5 +39,17 @@ public class User extends BaseEntity {
 
     private String postcode;
 
+    @Builder
+    public User(String userId, String password, String salt, String email, String name, String mobile, EnumGender gender, String address, String postcode) {
+        this.userId = userId;
+        this.password = password;
+        this.salt = salt;
+        this.email = email;
+        this.name = name;
+        this.mobile = mobile;
+        this.gender = gender;
+        this.address = address;
+        this.postcode = postcode;
+    }
 
 }
